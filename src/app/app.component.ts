de90cce07c47fe5a632bb4dc6628263215e1e972
Jack Pritom Soren <jackpritomsoren@hotmail.com>
2024-03-28 10:24:04 +0600

    Using Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HeaderComponent],
+  imports: [RouterOutlet,HeaderComponent,RecipesComponent,RecipeDetailComponent,RecipeItemComponent,RecipeListComponent,ShoppingEditComponent,ShoppingListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { RecipeListComponent } from './recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './recipe-list/recipe-item/recipe-item.component';
 
 @Component({
   selector: 'app-recipes',
   standalone: true,
-  imports: [],
+  imports: [RecipeListComponent,RecipeDetailComponent,RecipeItemComponent],
   templateUrl: './recipes.component.html',
   styleUrl: './recipes.component.css'
 })
